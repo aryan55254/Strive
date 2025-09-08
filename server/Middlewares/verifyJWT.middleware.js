ecd063db9210822d62215e4b6b93ecb85e8c2f08
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-09-08 23:59:43 +0530

    auth middleware built with propper error handling

---
+++ server/Middlewares/verifyJWT.middleware.js
@@ -0,0 +1,37 @@
+const User = require("../Models/User.model");
+require("dotenv").config();
+const SECRETKEY = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
+
+const authmiddleware = async (req, res, next) => {
+  let token;
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    token = authHeader.split(" ")[1];
+  } else {
+    return res.status(401).json({
+      message: "Unauthorized: No token provided or Wrong Token Provided",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, SECRETKEY);
+
+    const userid = decoded.userid;
+    const user = await User.findById(userid, "_id Email Username");
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Not authorized, user not found" });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    return res
+      .status(403)
+      .json({ message: "Forbidden: Invalid or expired token" });
+  }
+};
+
+module.exports = authmiddleware;
