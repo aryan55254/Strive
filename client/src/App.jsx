b9e1cc72af933c943cda8fc5f1296f8266d1076a
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-09-15 02:22:09 +0530

    feat: initial routing built

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SavedDiets from "./pages/SavedDiets";
+import SavedWorkouts from "./pages/SavedWorkouts";
+import GenerateDiet from "./pages/GenenerateDiet";
+import GenerateWorkout from "./pages/GenerateWorkout";
+
 function App() {
-  return <div className="bg-indigo-300">App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/saveddiets" element={<SavedDiets />}></Route>
+      <Route path="/savedworkouts" element={<SavedWorkouts />}></Route>
+      <Route path="/generatediet" element={<GenerateDiet />}></Route>
+      <Route path="/generateworkout" element={<GenerateWorkout />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
